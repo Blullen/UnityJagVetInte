69127ba2c6261baaccb2475350c6e7785b13fee1
BigBoyFizzy <elias.elmefors@gmail.com>
2026-03-25 09:59:09 +0100

    Bean movement
    Det går skitt

--- Assets/PlayerCam.cs
+++ Assets/PlayerCam.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
 
 public class PlayerCam : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
+public float sensX;
+public float sensY;
+
+public transform orientation;
+
+float xRotation;
+float yRotation;
+
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+    //get mouse input
+    float mouseX = input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+    float mouseY = input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+    yRotation += mouseX;
+
+    xRotation -= mouseY;
+    xRotation = Mathf.Clmap(xRotation, -90f, 90f);
+
+    //rotate cam and orietation
+    transfrom.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+    orietation.roatation = Quaternion.Euler(0, yRotation, 0);   
     }
 }
