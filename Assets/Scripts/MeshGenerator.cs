f2ed6c363ae2cec301130914f9d6b3af8750e0a8
Blullen <lucas.lindqquist@gmail.com>
2026-03-26 09:44:40 +0100

    VS Code funkar!!!!!!

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -2,22 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshFilter))]
 public class MeshGenerator : MonoBehaviour {
 
     Mesh mesh;
 
     Vector3[] vertices;
-    int[] triangels;
+    int[] triangles;
 
 
     void Start()
     {
         mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
+        UpdateMesh();
     }
 
-    
-    void Update()
+
+    void CreateShape()
     {
-        
+        vertices = new Vector3[]
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(0, 0, 1),
+            new Vector3(1, 0, 0),
+        };
+
+        triangles = new int[]
+        {
+            0, 1, 2
+        };
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
     }
 }
