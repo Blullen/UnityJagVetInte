196d0ab83e35972fb4b0c48eb4f8ed18140d25fb
Blullen <lucas.lindqquist@gmail.com>
2026-04-01 16:34:25 +0200

    oooo stor kvadrat med bollar

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -10,30 +10,60 @@ public class MeshGenerator : MonoBehaviour {
     Vector3[] vertices;
     int[] triangles;
 
+    public int xSize = 20;
+    public int zSize = 20;
 
     void Start()
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
 
-        CreateShape();
+        StartCoroutine(CreateShape());
+    }
+
+    private void Update()
+    {
         UpdateMesh();
     }
 
 
-    void CreateShape()
+    IEnumerator CreateShape()
     {
-        vertices = new Vector3[]
-        {
-            new Vector3(0, 0, 0),
-            new Vector3(0, 0, 1),
-            new Vector3(1, 0, 0),
-        };
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
 
-        triangles = new int[]
+        for (int i = 0, z = 0; z <= zSize; z++)
         {
-            0, 1, 2
-        };
+            for (int x = 0; x <= xSize; x++)
+            {
+                vertices[i] = new Vector3(x, 0, z);
+                i++;
+            }
+        }
+
+        triangles = new int[xSize * zSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+
+                yield return new WaitForSeconds(.1f);
+
+                // Börja videon på 10:20 ish //
+            }
+        }
     }
 
     void UpdateMesh()
@@ -42,5 +72,18 @@ public class MeshGenerator : MonoBehaviour {
 
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+            return;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], .1f);
+        }
     }
 }
